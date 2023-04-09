8506b841a9c97dff5fded42c00cf75455f85ee0d
Tegar Shallahudin Al Ayubi <tegarshallahudin1@gmail.com>
2023-04-10 01:15:47 +0700

    update home done

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -28,6 +28,8 @@
   <!-- Template Main CSS File -->
   <link href="{{ asset('') }}frontend/assets/css/style.css" rel="stylesheet">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <!-- =======================================================
   * Template Name: Arsha
   * Updated: Mar 10 2023 with Bootstrap v5.2.3
