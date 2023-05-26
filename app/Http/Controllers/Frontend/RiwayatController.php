6c1aed2720c220a9ba01389d299e157f53a47c5a
Aulia Novrin Harleyanto <novrinaulia@gmail.com>
2023-05-26 17:03:27 +0700

    pesan error update password

--- app/Http/Controllers/Frontend/RiwayatController.php
+++ app/Http/Controllers/Frontend/RiwayatController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ModelPasien;
 
 class RiwayatController extends Controller
 {
     public function riwayat()
     {
-        return view('layouts.frontend.riwayat.riwayat');
+        $data = ModelPasien::where('id', session('id'))->first();
+        return view('layouts.frontend.riwayat.riwayat', compact('data'));
     }
 }
